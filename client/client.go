34e916ac773046005c82de561625fa353bd89048
Ivan Aracki <aracki.ivan@gmail.com>
2018-01-27 18:49:01 +0100

    Remove log.fatal from client.go

--- client/client.go
+++ client/client.go
@@ -16,21 +16,23 @@ import (
 
 // getTokenFromWeb uses Config to request a Token.
 // It returns the retrieved Token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+func getTokenFromWeb(config *oauth2.Config) (*oauth2.Token, error) {
 	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
 	fmt.Printf("Go to the following link in your browser then type the "+
 		"authorization code: \n%v\n", authURL)
 
 	var code string
 	if _, err := fmt.Scan(&code); err != nil {
-		log.Fatalf("Unable to read authorization code %v", err)
+		log.Printf("Unable to read authorization code %v", err)
+		return nil, err
 	}
 
 	tok, err := config.Exchange(oauth2.NoContext, code)
 	if err != nil {
-		log.Fatalf("Unable to retrieve token from web %v", err)
+		log.Printf("Unable to retrieve token from web %v", err)
+		return nil, err
 	}
-	return tok
+	return tok, nil
 }
 
 // tokenCacheFile generates credential file path/filename.
@@ -61,27 +63,35 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 
 // saveToken uses a file path to create a file and store the
 // token in it.
-func saveToken(file string, token *oauth2.Token) {
+func saveToken(file string, token *oauth2.Token) error {
 	fmt.Printf("Saving credential file to: %s\n", file)
 	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
+		log.Printf("Unable to cache oauth token: %v", err)
+		return err
 	}
 	defer f.Close()
 	json.NewEncoder(f).Encode(token)
+
+	return nil
 }
 
 // getClient uses a Context and Config to retrieve a Token
 // then generate a Client. It returns the generated Client.
-func GetClient(ctx context.Context, config *oauth2.Config) *http.Client {
+func GetClient(ctx context.Context, config *oauth2.Config) (*http.Client, error) {
 	cacheFile, err := tokenCacheFile()
 	if err != nil {
-		log.Fatalf("Unable to get path to cached credential file. %v", err)
+		log.Printf("Unable to get path to cached credential file. %v", err)
+		return nil, err
 	}
 	tok, err := tokenFromFile(cacheFile)
 	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(cacheFile, tok)
+		if tok, err = getTokenFromWeb(config); err != nil {
+			return nil, err
+		}
+		if err := saveToken(cacheFile, tok); err != nil {
+			return nil, err
+		}
 	}
-	return config.Client(ctx, tok)
+	return config.Client(ctx, tok), nil
 }
